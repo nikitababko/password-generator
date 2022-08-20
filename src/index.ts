15661b8cbe25710223d68149800c14d46af39477
Nikita Babko <niki.babko@gmail.com>
2022-08-20 22:42:24 +0700

    Added some methods and setup config

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+const alphabetUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const alphabetLowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = `!";#$%&'()*+,-./:;<=>?@[]^_{|}~`;
+
+const random = (args: any) => {
+    const { only } = args;
+    const value = only ? 10 : 16;
+    const randomIndex = Number(String(Math.random())[2]);
+
+    return (
+        numbers[randomIndex] + alphabetLowerCase[randomIndex] + alphabetUpperCase[randomIndex] + symbols[randomIndex]
+    );
+};
+
+const passGen = (args: any): string => {
+    const { length, only } = args;
+
+    const pattern = Array(length).fill('#');
+    const password = pattern.join('').replaceAll(/#/g, () => random({ only }));
+
+    return password.slice(0, 16);
+};
+
+console.log(passGen({ length: 16, only: true, quantity: 3 }));
