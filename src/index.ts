b9349ba80f79f465ed077dfc01ad87bbe6b7aa79
Nikita Babko <niki.babko@gmail.com>
2022-08-21 19:23:29 +0700

    Added includes abilities

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,104 @@
 const alphabetUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const alphabetLowerCase = 'abcdefghijklmnopqrstuvwxyz';
-const numbers = '0123456789';
-const symbols = `!";#$%&'()*+,-./:;<=>?@[]^_{|}~`;
+const alphabetNumbers = '0123456789';
+const alphabetSymbols = `!";#$%&'()*+,-./:;<=>?@[]^_{|}~`;
+const similarCharacters = 'ilI1LoO0';
+
+const stringDontBeginNumberOrLetter = (str: string) => {
+    const firstChar = str[0];
+
+    const withANumber = () => {
+        for (let item of alphabetNumbers) {
+            if (item === firstChar) return !!item;
+        }
+    };
+
+    const withASymbol = () => {
+        for (let item of alphabetSymbols) {
+            if (item === firstChar) return !!item;
+        }
+    };
+
+    const newStr = withANumber() || withASymbol() ? str.replace(firstChar, 'j') : str;
+
+    return newStr;
+};
 
 const random = (args: any) => {
-    const { only } = args;
-    const value = only ? 10 : 16;
-    const randomIndex = Number(String(Math.random())[2]);
+    const {
+        noSimilarCharacters,
+        dontBeginWithANumberOrSymbol,
+        includeNumbers,
+        includeLowerCaseChar,
+        includeUpperCaseChar,
+        includeSymbols,
+    } = args;
+    const randomIndex = () => Number(String(Math.random())[2]);
 
-    return (
-        numbers[randomIndex] + alphabetLowerCase[randomIndex] + alphabetUpperCase[randomIndex] + symbols[randomIndex]
-    );
+    let str: string[] = [];
+
+    if (includeNumbers) str.push(alphabetNumbers[randomIndex()]);
+    if (includeLowerCaseChar) str.push(alphabetLowerCase[randomIndex()]);
+    if (includeUpperCaseChar) str.push(alphabetUpperCase[randomIndex()]);
+    if (includeSymbols) str.push(alphabetLowerCase[randomIndex()]);
+
+    return str.join('');
 };
 
-const passGen = (args: any): string => {
-    const { length, only, quantity } = args;
+const passGen = (args: any): string[] => {
+    const {
+        length,
+        only,
+        quantity,
+        noSimilarCharacters,
+        includeNumbers,
+        includeLowerCaseChar,
+        includeUpperCaseChar,
+        includeSymbols,
+        dontBeginWithANumberOrSymbol,
+    } = args;
 
     const pattern = Array(length).fill('#');
-    const password = pattern.join('').replaceAll(/#/g, () => random({ only }));
 
-    return password.slice(0, 16);
+    const password = (args: any): string[] => {
+        const { quantity, noSimilarCharacters, dontBeginWithANumberOrSymbol } = args;
+
+        const arr: string[] = [];
+
+        for (let i = 0; i < quantity; i++) {
+            const v = pattern
+                .join('')
+                .replaceAll(/#/g, () =>
+                    random({
+                        noSimilarCharacters,
+                        dontBeginWithANumberOrSymbol,
+                        includeNumbers,
+                        includeLowerCaseChar,
+                        includeUpperCaseChar,
+                        includeSymbols,
+                    }),
+                )
+                .slice(0, 16);
+
+            arr.push(v);
+        }
+
+        return arr;
+    };
+
+    return password({ quantity, noSimilarCharacters, dontBeginWithANumberOrSymbol, includeNumbers });
 };
 
-console.log(passGen({ length: 16, only: true, quantity: 3 }));
+console.log(
+    passGen({
+        length: 16,
+        only: true,
+        quantity: 3,
+        noSimilarCharacters: true,
+        dontBeginWithANumberOrSymbol: true,
+        includeNumbers: true,
+        includeLowerCaseChar: true,
+        includeUpperCaseChar: true,
+        includeSymbols: true,
+    }),
+);
