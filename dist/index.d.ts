9eca38f3a5be961b947efdb48ba333ad1c3cef7d
Nikita Babko <niki.babko@gmail.com>
2022-10-02 22:27:10 +0700

    Fixed JSDoc

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,34 +1,25 @@
 import { GeneratePasswordType } from 'index.types';
 /**
- * generatePassword.
+ * Password generator
  *
- * @param {number} [length=4] - passwords length
- * @param {number} [quantity=1] - passwords quantity
- * @param {boolean} includeNumbers - inlcude numbers or not
- * @param {boolean} dontStartWithANumber - do not start password with number
- * @param {boolean} dontStartWithASymbol - do not start password with symbol
- * @param {boolean} includeLowerCaseChars - inlcude lower case chars in passwords
- * @param {boolean} includeUpperCaseChars - inlcude upper case chars in passwords
- * @param {boolean} includeSymbols - inlcude symbols in password
- * @param {boolean} noSimilarChars - inlcude similar chars in password
- * @param {boolean} noSequentialChars - passwords without sequences
- * @param {boolean} noDuplicatesChars - passwords without dublicates
+ * @param {number} [length=6] passwords length
+ * @param {number} [quantity=1] passwords quantity
+ * @param {boolean} [includeNumbers] inlcude numbers or not
+ * @param {boolean} [dontStartWithANumber] do not start password with number
+ * @param {boolean} [dontStartWithASymbol] do not start password with symbol
+ * @param {boolean} [includeLowerCaseChars] inlcude lower case chars in passwords
+ * @param {boolean} [includeUpperCaseChars] inlcude upper case chars in passwords
+ * @param {boolean} [includeSymbols] inlcude symbols in password
+ * @param {boolean} [noSimilarChars] inlcude similar chars in password
+ * @param {boolean} [noSequentialChars] passwords without sequences
+ * @param {boolean} [noDuplicatesChars] passwords without dublicates
  * @return {string[]} passwords array
  *
  * @example
- *     generatePassword({
-                length,
-                quantity,
-                dontStartWithANumber: true,
-                dontStartWithASymbol: true,
-                includeLowerCaseChars: true,
-                includeNumbers: true,
-                includeSymbols: true,
-                includeUpperCaseChars: true,
-                noDuplicatesChars: true,
-                noSequentialChars: true,
-                noSimilarChars: true,
-            })
+ * generatePassword({length = 16, quantity = 2, dontStartWithANumber: true, dontStartWithASymbol: true, includeLowerCaseChars: true, includeNumbers: true, includeSymbols: true, includeUpperCaseChars: true, noDuplicatesChars: true, noSequentialChars: true, noSimilarChars: true})
+ *
+ * @author Nikita Babko <niki.babko@gmail.com>
+ * @link <https://github.com/nikitababko>
  */
 export declare const generatePassword: GeneratePasswordType;
 //# sourceMappingURL=index.d.ts.map
