228621e69c3b019832a68326df93b5155eae091c
Nikita Babko <niki.babko@gmail.com>
2022-10-03 10:53:01 +0700

    Fixed tsconfig and index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,15 @@ import {
  *
  * @param {number} [length=6] passwords length
  * @param {number} [quantity=1] passwords quantity
- * @param {boolean} [includeNumbers] inlcude numbers or not
+ * @param {boolean} [includeNumbers] include numbers or not
  * @param {boolean} [dontStartWithANumber] do not start password with number
  * @param {boolean} [dontStartWithASymbol] do not start password with symbol
- * @param {boolean} [includeLowerCaseChars] inlcude lower case chars in passwords
- * @param {boolean} [includeUpperCaseChars] inlcude upper case chars in passwords
- * @param {boolean} [includeSymbols] inlcude symbols in password
- * @param {boolean} [noSimilarChars] inlcude similar chars in password
+ * @param {boolean} [includeLowerCaseChars] include lower case chars in passwords
+ * @param {boolean} [includeUpperCaseChars] include upper case chars in passwords
+ * @param {boolean} [includeSymbols] include symbols in password
+ * @param {boolean} [noSimilarChars] include similar chars in password
  * @param {boolean} [noSequentialChars] passwords without sequences
- * @param {boolean} [noDuplicatesChars] passwords without dublicates
+ * @param {boolean} [noDuplicatesChars] passwords without duplicates
  * @return {string[]} passwords array
  *
  * @example
